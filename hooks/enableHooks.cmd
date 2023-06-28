539e7cb4d69306379c2555e10fc2c42899f699f4
Vladimir Pestov <92364726+BasedDepartment1@users.noreply.github.com>
2023-06-28 22:25:27 +0500

    Update enableHooks.cmd

--- hooks/enableHooks.cmd
+++ hooks/enableHooks.cmd
@@ -2,4 +2,4 @@ curl ^
   --request POST ^
   --url https://api.telegram.org/bot<TG_TOKEN>/setWebhook ^
   --header "content-type: application/json" ^
-  --data "{\"url\": \"https://functions.yandexcloud.net/<FUNCTION_ID>/<FUNCTION_NAME>\"}"
+  --data "{\"url\": \"https://functions.yandexcloud.net/<FUNCTION_ID>\"}"
