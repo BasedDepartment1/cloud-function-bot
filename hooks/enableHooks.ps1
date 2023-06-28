0d1d3244cf547271ef4e2c3c36c770674b0ac345
Vladimir Pestov <92364726+BasedDepartment1@users.noreply.github.com>
2023-06-28 22:26:00 +0500

    Update enableHooks.ps1

--- hooks/enableHooks.ps1
+++ hooks/enableHooks.ps1
@@ -2,4 +2,4 @@ curl.exe `
   --request POST `
   --url https://api.telegram.org/bot<TG_TOKEN>/setWebhook `
   --header '"content-type: application/json"' `
-  --data '"{ \"url\": \"https://functions.yandexcloud.net/<FUNCTION_ID>/<FUNCTION_NAME>\" }"'
+  --data '"{ \"url\": \"https://functions.yandexcloud.net/<FUNCTION_ID>\" }"'
