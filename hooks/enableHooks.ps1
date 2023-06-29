32fec7d88a93c7175fdde376e580b0198b8c72de
Vladimir_Pestov <Vladimir.Pestov@urfu.me>
2023-06-30 02:08:49 +0500

    Use CLI script to enable webhooks

--- hooks/enableHooks.ps1
+++
@@ -1,5 +0,0 @@
-curl.exe `
-  --request POST `
-  --url https://api.telegram.org/bot<TG_TOKEN>/setWebhook `
-  --header '"content-type: application/json"' `
-  --data '"{ \"url\": \"https://functions.yandexcloud.net/<FUNCTION_ID>\" }"'
